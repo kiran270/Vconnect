72e946b800c5471987d5742b42f9c01dd1436300
kiran270 <kirangilli.kumar@gmail.com>
2019-04-16 01:16:27 +0530

    MySQL Db connection

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var app=express();
 app.listen(5000);
 var mysql=require('mysql');
 app.use(express.static(__dirname + '/images'));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use(express.static(__dirname + '/JS_And_CSS'));
 app.set('view engine', 'ejs');
 var bodyParser = require('body-parser');
 app.use(bodyParser());
@@ -14,8 +12,22 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'kumar',
-  database : 'cricket'
+  database : 'vconnect'
 });
 app.get('/',function(req,res){
-	res.render('forgotpassword');
+	res.render('forgotpassword',{error:false});
+});
+app.post('/verifyEmpid',function(req,res){
+	var empid=req.body.empid;
+	var query='select * from users where username= '+empid;
+	connection.query('select * from users',function(err,users){
+		if(err) throw err;
+		else{
+			if(users.length==1){
+				res.render('securityquestions',{empid:empid});
+			}else{
+				res.render('forgotpassword',{error:true,errormessage:"Please check your Employee ID"});
+			}
+		}
+	});
 });
