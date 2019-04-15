8cf89b04b2873760a52c8b6024b3e3cd4a07797d
kiran270 <kirangilli.kumar@gmail.com>
2019-04-16 02:08:10 +0530

    flow flow with Db connection

--- server.js
+++ server.js
@@ -20,13 +20,45 @@ app.get('/',function(req,res){
 app.post('/verifyEmpid',function(req,res){
 	var empid=req.body.empid;
 	var query='select * from users where username= '+empid;
-	connection.query('select * from users',function(err,users){
+	connection.query(query,function(err,users){
 		if(err) throw err;
 		else{
 			if(users.length==1){
-				res.render('securityquestions',{empid:empid});
+				res.render('securityquestions',{error:false,empid:empid});
 			}else{
 				res.render('forgotpassword',{error:true,errormessage:"Please check your Employee ID"});
+			}
+		}
+	});
+});
+app.post('/verifysecurityanswers',function(req,res){
+	var empid=req.body.empid;
+	console.log(empid)
+	var sa1=req.body.sa1;
+	var sa2=req.body.sa2;
+	console.log(sa1)
+	console.log(sa2)
+	var query='select * from users where username= ? AND answer1 = ? AND answer2= ?'
+	connection.query(query,[empid,sa1,sa2],function(err,users){
+		if(err) throw err;
+		else{
+			if(users.length==1){
+				res.render('changepassword',{empid:empid});
+			}else{
+				res.render('securityquestions',{empid:empid,error:true,errormessage:"Invalid Security Answers"});
+			}
+		}
+	});
+});
+app.post('/changepassword',function(req,res){
+	var empid=req.body.empid;
+	var password=req.body.newpassword;
+	var query="UPDATE users SET password = ? WHERE username = ?";
+	connection.query(query,[password,empid],function(err,result){
+		if(err) throw err;
+		else{
+			if(result.affectedRows==1){
+				res.render('success',{error:false});
 			}
 		}
 	});
